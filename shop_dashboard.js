20c5916b1b1c8e932730c6d9be7106b5025e1b7a
ESSReport <pacada.erl@gmail.com>
2026-07-01 20:45:21 +0800

    Update shop_dashboard.js

--- shop_dashboard.js
+++ shop_dashboard.js
@@ -8,8 +8,8 @@ const SHEETS = {
   STLM: `https://opensheet.elk.sh/${SHEET_ID}/STLM%2FTOPUP`,
   COMM: `https://opensheet.elk.sh/${SHEET_ID}/COMM`,
   SHOP_BALANCE: `https://opensheet.elk.sh/${SHEET_ID}/SHOPS%20BALANCE`,
-  WALLET_DP: `https://opensheet.elk.sh/1kYauM_lC-6EFmzGy0w4ansnWb5rVmF5oiJAdhkze6fE/DP`,
-  WALLET_WD: `https://opensheet.elk.sh/1kYauM_lC-6EFmzGy0w4ansnWb5rVmF5oiJAdhkze6fE/WD`
+  WALLET_DP: `https://opensheet.elk.sh/1lYGfU_-ZEj1Bwp6Ocrn3Vkd2mYmxdi8WLOB8AhWsRHE/DP`,
+  WALLET_WD: `https://opensheet.elk.sh/1lYGfU_-ZEj1Bwp6Ocrn3Vkd2mYmxdi8WLOB8AhWsRHE/WD`
 };
 
 const shopName = new URLSearchParams(window.location.search).get("shopName") || "";
