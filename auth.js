e464603a939f0bcaead345efa466331894ed1f2d
ESSReport <pacada.erl@gmail.com>
2026-07-13 10:39:10 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -80,7 +80,8 @@ const TL_PINS = {
    "TL-MEHRA": "875713",
    "TL-RAJU": "876814",
    "TL-MOHDDIN": "876994",
-   "TL-TANVIR": "879644"
+   "TL-TANVIR": "879644",
+   "TL-MIRAAN": "879644"
 
 };
 
